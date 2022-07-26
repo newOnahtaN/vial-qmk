64bc452f7ed6a6a1100e2e425b11abba4a8e928d
newOnahtaN <nate.cole.owen@gmail.com>
2022-07-25 17:34:37 -0700

    Initial commit from naowen

---
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x69, 0x46, 0x63, 0xE3, 0xF4, 0x9C, 0x99, 0xB6}
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+//#define MASTER_RIGHT
+
+//#define EE_HANDS
--- keyboards/handwired/dactyl_manuform/6x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/6x6/rules.mk
@@ -7,9 +7,10 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = handwired/dactyl_manuform/6x6/promicro
+
